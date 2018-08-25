78315e257809068d172175a9df12ecfc9c1bd306
DevMountain <devmountain@devmountains-macbook-pro.local>
2018-08-25 09:28:54 -0600

    Initial Commit

---
+++ myAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myAlarm
+//
+//  Created by DevMountain on 8/25/18.
+//  Copyright © 2018 trevorAdcock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
