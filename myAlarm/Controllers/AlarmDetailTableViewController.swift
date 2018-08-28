838c81ce99062f882b76000f94860054d0d60cf8
DevMountain <devmountain@DevMountains-MacBook-Pro.local>
2018-08-28 16:38:27 -0600

    Fixes bug when toggling button

--- myAlarm/Controllers/AlarmDetailTableViewController.swift
+++ myAlarm/Controllers/AlarmDetailTableViewController.swift
@@ -61,6 +61,7 @@ class AlarmDetailTableViewController: UITableViewController, AlarmScheduler {
     @IBAction func alarmEnabledButtonTapped(_ sender: UIButton) {
         guard let alarm = alarm else {return}
         AlarmController.shared.toggleEnabled(for: alarm)
+        alarmIsOn = alarm.enabled
         setUpAlarmButton()
     }
     
